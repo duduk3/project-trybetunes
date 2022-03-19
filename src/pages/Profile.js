127f25adfb1df0ba96609d6c5fdd827d77895911
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-03-18 22:57:50 -0300

    req13 => insere dados usuario

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
-  render() {
-    return (
-      <main data-testid="page-profile">
-        <Header />
-      </main>
-    );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      user: {},
+    };
   }
+
+  componentDidMount() {
+    this.getProfile();
+  }
+
+getProfile = async () => {
+  this.setState({ loading: true });
+  const user = await getUser();
+  this.setState({ user });
+  this.setState({ loading: false });
+}
+
+render() {
+  const { loading, user } = this.state;
+  return (
+    <main data-testid="page-profile">
+      { loading && <Loading /> }
+      {
+        <div>
+          <Header />
+          <div>
+            <img
+              src={ user.image }
+              alt="foto-do-perfil"
+              data-testid="profile-image"
+            />
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+          <p>{ user.name }</p>
+          <p>{ user.email }</p>
+          <p>{ user.description }</p>
+        </div>
+      }
+    </main>
+  );
+}
 }
 
 export default Profile;
